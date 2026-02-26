80ced81c71acd7db432a169af529ae19ca3771d7
ThunderVolt45 <zxc9876zxc@gmail.com>
2026-02-26 17:51:40 +0900

    Initial commit

---
+++ Source/NovaRevolution/NovaRevolution.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NovaRevolution.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NovaRevolution, "NovaRevolution" );
+
+DEFINE_LOG_CATEGORY(LogNovaRevolution)
+ 
---
+++ Source/NovaRevolution/NovaRevolutionGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NovaRevolutionGameMode.h"
+
+ANovaRevolutionGameMode::ANovaRevolutionGameMode()
+{
+	// stub
+}
