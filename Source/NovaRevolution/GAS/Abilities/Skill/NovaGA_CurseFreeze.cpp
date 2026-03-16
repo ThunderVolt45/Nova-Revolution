d7978e8084913d57f02a06cf0b97328c6a675b04
SeongAA-Park <seongah0201@gmail.com>
2026-03-16 16:28:49 +0900

    Feat: 저주 프리즈(Curse Freeze) 액티브 스킬 GA 구현
    - NovaGA_CurseFreeze: NovaTargetActor_GroundRadius를 이용한 적군 범위 타겟팅 로직 구현
    - NovaGA_CurseFreeze: 타겟 확정 시 GE_CurseFreeze_Movement 적용 및 자원(Watt/SP) 소모 처리
    - NovaGA_CurseFreeze: 스킬 실행 시 PlayerController를 스킬 모드로 전환하는 예외 처리 추가

---
+++ Source/NovaRevolution/GAS/Abilities/Skill/NovaGA_CurseFreeze.cpp
@@ -0,0 +1,124 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GAS/Abilities/Skill/NovaGA_CurseFreeze.h"
+#include "GAS/Targeting/NovaTargetActor_GroundRadius.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
+#include "NovaRevolution.h"
+#include "Abilities/Tasks/AbilityTask_WaitTargetData.h"
+#include "Core/NovaUnit.h"
+#include "Core/NovaLog.h"
+#include "Player/NovaPlayerController.h"
+
+UNovaGA_CurseFreeze::UNovaGA_CurseFreeze()
+{
+    // 요구사항: Watt 200, SP 20 (부모 클래스인 NovaSkillAbility의 변수 사용)
+    WattCost = 200.0f;
+    SPCost = 20.0f;
+
+    InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+}
+
+void UNovaGA_CurseFreeze::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+    Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+    // 1. 자원 체크 (Watt/SP 부족 시 종료)
+    if (!CheckSkillCost())
+    {
+        EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+        return;
+    }
+
+    // 2. 컨트롤러를 스킬 모드로 전환 (마우스 커서 변경 및 다른 클릭 무시용)
+    if (ANovaPlayerController* PC = Cast<ANovaPlayerController>(GetActorInfo().PlayerController))
+    {
+        PC->SetPendingCommandType(ECommandType::Skill);
+    }
+
+    // 3. 타겟 액터 스폰 및 설정
+    if (!TargetActorClass)
+    {
+        NOVA_SCREEN(Error, "TargetActorClass가 설정되지 않았습니다!");
+        EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+        return;
+    }
+
+    ANovaTargetActor_GroundRadius* SpawnedTargetActor = GetWorld()->SpawnActor<ANovaTargetActor_GroundRadius>(TargetActorClass);
+    if (SpawnedTargetActor)
+    {
+        SpawnedTargetActor->StartLocation = MakeTargetLocationInfoFromOwnerActor();
+        SpawnedTargetActor->Radius = FreezeRadius;
+
+        // [중요] 필터를 적군(Enemy)으로 설정! 아군 하이라이트와 차별화
+        SpawnedTargetActor->TargetingFilter = ENovaTargetingFilter::Enemy;
+        SpawnedTargetActor->CurrentHighlightColor = SkillHighlightColor;
+    }
+
+    // 4. 타겟 데이터 대기 태스크 (UserConfirmed: 좌클릭 시 확정)
+    UAbilityTask_WaitTargetData* WaitTargetTask = UAbilityTask_WaitTargetData::WaitTargetDataUsingActor(
+        this,
+        TEXT("WaitTargetData_CurseFreeze"),
+        EGameplayTargetingConfirmation::UserConfirmed,
+        SpawnedTargetActor
+    );
+
+    if (WaitTargetTask)
+    {
+        WaitTargetTask->ValidData.AddDynamic(this, &UNovaGA_CurseFreeze::OnTargetDataReadyCallback);
+        WaitTargetTask->Cancelled.AddDynamic(this, &UNovaGA_CurseFreeze::OnTargetDataCancelledCallback);
+        WaitTargetTask->ReadyForActivation();
+    }
+}
+
+void UNovaGA_CurseFreeze::OnTargetDataReadyCallback(const FGameplayAbilityTargetDataHandle& DataHandle)
+{
+    // 1. 자원 소모
+    ApplySkillCost();
+
+    // 2. 타겟 유닛들에게 효과 적용
+    TArray<AActor*> TargetActors = UAbilitySystemBlueprintLibrary::GetAllActorsFromTargetData(DataHandle);
+
+    for (AActor* Actor : TargetActors)
+    {
+        if (ANovaUnit* Unit = Cast<ANovaUnit>(Actor))
+        {
+            // GE_CurseFreeze 적용 (속도 0, State_Frozen 태그 부여)
+            if (CurseFreezeGEClass)
+            {
+                FGameplayEffectSpecHandle SpecHandle = MakeOutgoingGameplayEffectSpec(CurseFreezeGEClass);
+                if (SpecHandle.IsValid())
+                {
+                    ApplyGameplayEffectSpecToTarget(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, SpecHandle, DataHandle);
+                }
+            }
+
+            // 시각 효과 실행 (GameplayCue)
+            if (FreezeCueTag.IsValid() && Unit->GetAbilitySystemComponent())
+            {
+                Unit->GetAbilitySystemComponent()->ExecuteGameplayCue(FreezeCueTag);
+            }
+        }
+    }
+
+    EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+}
+
+void UNovaGA_CurseFreeze::OnTargetDataCancelledCallback(const FGameplayAbilityTargetDataHandle& DataHandle)
+{
+    EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
+}
+
+void UNovaGA_CurseFreeze::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+    // 어빌리티 종료 시 컨트롤러 상태 복구
+    if (ANovaPlayerController* PC = Cast<ANovaPlayerController>(ActorInfo->PlayerController))
+    {
+        if (PC->GetPendingCommandType() == ECommandType::Skill)
+        {
+            PC->SetPendingCommandType(ECommandType::None);
+        }
+    }
+
+    Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
